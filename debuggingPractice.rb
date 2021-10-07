d5d65a590a290063de0dd219139e64c982a4d6ae
Leon <lmshimizu@gmail.com>
2021-10-06 17:06:08 -0700

    debugging practice

---
+++ debuggingPractice.rb
@@ -0,0 +1,17 @@
+# puts "Hello!"
+# greeting = gets.chomp
+# if greeting == "Arrr!"
+#   puts "Go away, pirate."
+# else 
+#   puts "Greetings, hater of pirates!"
+# end
+
+# puts "Greetings! What is your year of origin?"
+# origin = gets.chomp.to_i
+# if origin < 1900
+#   puts "That’s the past!"
+# elsif origin > 1900 && origin < 2020
+#   puts "That’s the present!"
+# else
+#   puts "That’s the future!"
+# end
